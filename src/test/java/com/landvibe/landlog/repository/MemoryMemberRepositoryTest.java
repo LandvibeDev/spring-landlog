4f38e8c82d8c731436bfbe71b279af9311a01faa
leesewon00 <dltpdnjs2000@gmail.com>
2023-07-16 13:39:21 +0900

    add test code

--- src/test/java/com/landvibe/landlog/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/com/landvibe/landlog/repository/MemoryMemberRepositoryTest.java
@@ -2,7 +2,6 @@ package com.landvibe.landlog.repository;
 
 import com.landvibe.landlog.domain.Member;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -67,40 +66,23 @@ class MemoryMemberRepositoryTest {
         assertThat(result.size()).isEqualTo(2);
     }
 
-    private Member createMember() {
+    @Test
+    public void findByEmailAndPassword(){
+        //given
         Member member1 = new Member();
         member1.setName("name1");
         member1.setEmail("email1");
         member1.setPassword("password1");
         repository.save(member1);
-        return member1;
-    }
-
-    @Test
-    @DisplayName("이메일과 비밀번호로 회원 조회 성공 테스트")
-    public void findByEmailAndPassword_success(){
-        //given
-        Member member1 = createMember();
 
         //when
-        Optional<Member> successResult = repository
-                .findByEmailAndPassword("email1", "password1");
+        Optional<Member> successResult = repository.findByEmailAndPassword("email1", "password1");
+        Optional<Member> failedResult = repository.findByEmailAndPassword("email2", "password2");
 
         //then
+        assertThat(successResult).isPresent();
         assertThat(successResult.get()).isEqualTo(member1);
-    }
 
-    @Test
-    @DisplayName("이메일과 비밀번호로 회원 조회 실패 테스트")
-    public void findByEmailAndPassword_fail(){
-        //given
-        Member member1 = createMember();
-
-        //when
-        Optional<Member> failedResult = repository
-                .findByEmailAndPassword("email2", "password2");
-
-        //then
         assertThat(failedResult).isEmpty();
     }
 }
--- src/test/java/com/landvibe/landlog/service/MemberServiceTest.java
+++ src/test/java/com/landvibe/landlog/service/MemberServiceTest.java
@@ -4,10 +4,9 @@ import com.landvibe.landlog.domain.Member;
 import com.landvibe.landlog.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.landvibe.landlog.ErrorMessage.*;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -40,7 +39,6 @@ class MemberServiceTest {
         Member findMember = memberRepository.findById(saveId).get();
         assertEquals(member.getName(), findMember.getName());
     }
-
     @Test
     public void 중복_회원_예외() throws Exception {
         //Given
@@ -52,51 +50,26 @@ class MemberServiceTest {
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class,
                 () -> memberService.join(member2));//예외가 발생해야 한다.
-        assertThat(e.getMessage()).isEqualTo(EXIST_MEMBER.message);
-    }
-
-    private Member createMember() {
-        Member member = new Member("name", "email", "password");
-        memberService.join(member);
-        return member;
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
 
     @Test
-    @DisplayName("[로그인 성공] 이메일, 패스워드 일치")
-    public void login_success() {
+    public void login() throws Exception{
         //Given
-        Member member = createMember();
+        Member member1 = new Member();
+        member1.setName("name1");
+        member1.setEmail("email1");
+        member1.setPassword("password1");
+        memberService.join(member1);
 
         //When
-        Long successResult = memberService.login("email", "password");
+        Optional<Member> successResult = memberService.login("email1", "password1");
+        Optional<Member> failedResult = memberService.login("email2", "password2");
 
         //Then
-        assertThat(successResult).isEqualTo(member.getId());
-    }
+        assertThat(successResult).isPresent();
+        assertThat(successResult.get()).isEqualTo(member1);
 
-    @Test
-    @DisplayName("[로그인 실패] 이메일 불일치")
-    public void login_fail_wrong_email() {
-        //
-        Member member = createMember();
-
-        //
-        Exception exception = assertThrows(Exception.class, () -> {
-            memberService.login("wrong_email", "password");
-        });
-        assertThat(exception.getMessage()).isEqualTo(WRONG_EMAIL.message);
-    }
-
-    @Test
-    @DisplayName("[로그인 실패] 패스워드 불일치")
-    public void login_fail_wrong_password() {
-        //
-        Member member = createMember();
-
-        //
-        Exception exception = assertThrows(Exception.class, () -> {
-            memberService.login("email", "wrong_password");
-        });
-        assertThat(exception.getMessage()).isEqualTo(WRONG_PASSWORD.message);
+        assertThat(failedResult).isEmpty();
     }
 }
