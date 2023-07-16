4d497f63681ad5c47b71175ad2b91539275a8174
gimdongha <d11210920@gmail.com>
2023-07-16 10:52:12 +0900

    refactor(MemberController):add email and password logic and login logic

--- src/main/java/com/landvibe/landlog/controller/MemberController.java
+++ src/main/java/com/landvibe/landlog/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class MemberController {
@@ -26,6 +27,8 @@ public class MemberController {
     public String create(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
+        member.setEmail(form.getEmail());
+        member.setPassword(form.getPassword());
         memberService.join(member);
         return "redirect:/";
     }
@@ -35,5 +38,18 @@ public class MemberController {
         List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
         return "members/memberList";
+    }
+    @GetMapping(value = "/members/login")
+    public String loginForm(Model model) {
+        return "members/loginForm";
+    }
+    @PostMapping(value = "/members/login")
+    public String login(MemberForm form,Model model) {
+        Optional<Member> member = memberService.login(form.getEmail(),form.getPassword());
+        if(!member.isEmpty()) {
+            model.addAttribute("name",member.get().getName());
+            return "/blogs";
+        }
+        return "home";
     }
 }
