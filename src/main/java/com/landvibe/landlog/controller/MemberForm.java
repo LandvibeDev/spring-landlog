f56ba52546d05075224a3b92247c46275b2bd4db
gimdongha <d11210920@gmail.com>
2023-07-16 10:52:19 +0900

    refactor(MemberController):add email and password

--- src/main/java/com/landvibe/landlog/controller/MemberForm.java
+++ src/main/java/com/landvibe/landlog/controller/MemberForm.java
@@ -3,6 +3,25 @@ package com.landvibe.landlog.controller;
 public class MemberForm {
     private String name;
 
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getName() {
         return name;
     }
