d4c127ba7fa2ba2dc351129f24eef040482388a9
gimdongha <d11210920@gmail.com>
2023-07-16 10:51:39 +0900

    refactor(Member):add email and password

--- src/main/java/com/landvibe/landlog/domain/Member.java
+++ src/main/java/com/landvibe/landlog/domain/Member.java
@@ -4,13 +4,15 @@ public class Member {
 
     private Long id;
     private String name;
+    private String email;
+    private String password;
 
-    public Member() {
-    }
 
-    public Member(Long id, String name) {
+    public Member(Long id, String name,String email, String password) {
         this.id = id;
         this.name = name;
+        this.email = email;
+        this.password = password;
     }
 
     public Long getId() {
@@ -27,5 +29,23 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Member() {
     }
 }
