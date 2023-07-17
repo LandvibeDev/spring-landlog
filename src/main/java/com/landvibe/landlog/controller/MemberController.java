6a17239ae65ac4653c39c8c11ec357198cb8ca1c
gimdongha <d11210920@gmail.com>
2023-07-17 15:57:48 +0900

    feat(MemberController):modify end line

--- src/main/java/com/landvibe/landlog/controller/MemberController.java
+++ src/main/java/com/landvibe/landlog/controller/MemberController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 public class MemberController {
     private final MemberService memberService;
 
-    public MemberController(MemberService memberService)  {
+    public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
