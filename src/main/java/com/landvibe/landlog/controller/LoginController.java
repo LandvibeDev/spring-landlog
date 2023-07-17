924aa660e6a5c09491bfb610788995a6f52320c3
leesc0839 <leesc0839@naver.com>
2023-07-17 10:12:42 +0900

    refactor(LoginController): LoginController 수정
    LoginService 의존관계 생성 및, login 함수 구현

--- src/main/java/com/landvibe/landlog/controller/LoginController.java
+++ src/main/java/com/landvibe/landlog/controller/LoginController.java
@@ -1,17 +1,36 @@
 package com.landvibe.landlog.controller;
 
+import com.landvibe.landlog.service.LoginService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
 
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
     @GetMapping("/members/login")
-    public String loginForm(){
+    public String loginForm() {
         return "members/loginForm";
     }
 
+    @PostMapping("/members/login")
+    public String login(LoginForm loginForm, RedirectAttributes redirectAttributes) {
+        Long memberId;
+        try {
+            memberId = loginService.login(loginForm);
+        } catch (IllegalArgumentException e) {
+            return "redirect:/";
+        }
 
+        redirectAttributes.addAttribute("createId", memberId);
+        return "redirect:/blogs/{creatorId}";
+    }
 
 }
