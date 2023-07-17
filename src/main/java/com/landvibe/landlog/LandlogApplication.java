4e157001070fad80fc3a1391b22c6d193ee92e2e
kwonyonghyun <rnjs990523@naver.com>
2023-07-17 20:19:39 +0900

    기능구현 완료 TODO:테스트 케이스 작성, 리팩토링

--- src/main/java/com/landvibe/landlog/LandlogApplication.java
+++ src/main/java/com/landvibe/landlog/LandlogApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LandlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LandlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LandlogApplication.class, args);
+    }
 
 }
