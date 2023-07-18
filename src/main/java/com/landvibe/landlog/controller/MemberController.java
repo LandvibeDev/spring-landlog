d5a38ed0af9df91f14edfdd9d0a1ae1741907541
kuk6933 <kuk6933@naver.com>
2023-07-18 12:59:22 +0900

    feat: add login function

--- src/main/java/com/landvibe/landlog/controller/MemberController.java
+++ src/main/java/com/landvibe/landlog/controller/MemberController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -23,9 +24,11 @@ public class MemberController {
     }
 
     @PostMapping(value = "/members/new")
-    public String create(MemberForm form) {
+    public String create(JoinForm form) {
         Member member = new Member();
         member.setName(form.getName());
+        member.setPassword(form.getPassword());
+        member.setEmail(form.getEmail());
         memberService.join(member);
         return "redirect:/";
     }
@@ -35,5 +38,21 @@ public class MemberController {
         List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
         return "members/memberList";
+    }
+
+    @GetMapping(value = "/members/login")
+    public String createLoginForm() {
+        return "login/loginForm";
+    }
+
+    @PostMapping(value = "/members/login")
+    public String login(LoginForm loginForm, RedirectAttributes redirectAttributes) throws Exception {
+        try {
+            Member member = memberService.login(loginForm);
+            redirectAttributes.addAttribute("creatorId", member.getId());
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        return "redirect:/blogs";
     }
 }
