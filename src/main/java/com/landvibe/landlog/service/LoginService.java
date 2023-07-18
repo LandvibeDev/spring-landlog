d9db4c033240978d5b5733f15e01dce07c12b424
leesc0839 <leesc0839@naver.com>
2023-07-19 05:17:58 +0900

    refactor(LoginService): LoginService 수정
    orElseThrow()가 Member를 리턴하기 때문에 하나의 로직으로 합치는게 가능합니다.

--- src/main/java/com/landvibe/landlog/service/LoginService.java
+++ src/main/java/com/landvibe/landlog/service/LoginService.java
@@ -3,7 +3,6 @@ package com.landvibe.landlog.service;
 import com.landvibe.landlog.controller.LoginForm;
 import com.landvibe.landlog.domain.Member;
 import com.landvibe.landlog.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -19,20 +18,15 @@ public class LoginService {
 
     public Long login(LoginForm loginForm) throws IllegalArgumentException {
         Optional<Member> member = memberRepository.findByEmail(loginForm.getEmail());
-        Member validMember = validateLogin(member, loginForm);
+        validateLogin(member, loginForm);
 
-        return validMember.getId();
+        return member.get().getId();
     }
 
-    private Member validateLogin(Optional<Member> member, LoginForm loginForm) {
-        Member validMember = member.orElseThrow(() -> {
-            throw new IllegalArgumentException("존재하지 않는 이메일 입니다.");
-        });
-
-        if (!validMember.getPassword().equals(loginForm.getPassword())) {
+    private void validateLogin(Optional<Member> member, LoginForm loginForm) {
+        if (!member.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 이메일입니다."))
+                .getPassword().equals(loginForm.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
-
-        return validMember;
     }
 }
