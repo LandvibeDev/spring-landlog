9356b95444a2c5f80713e2e34087b668cd3edda9
kuk6933 <kuk6933@naver.com>
2023-07-18 13:52:34 +0900

    refactor: make package dto

--- src/main/java/com/landvibe/landlog/domain/Member.java
+++ src/main/java/com/landvibe/landlog/domain/Member.java
@@ -7,13 +7,13 @@ public class Member {
     private String email;
     private String password;
 
-    public Member() {
-    }
-
     public Member(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
+    }
+
+    public Member() {
     }
 
     public Long getId() {
