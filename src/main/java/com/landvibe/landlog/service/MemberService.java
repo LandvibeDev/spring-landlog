5c8ce4afa91fc1a183ca24d52f8d9a543856fa2f
genius00hwan <qnfn120@gmail.com>
2023-07-19 20:12:56 +0900

    fix(MemberService) : add func to validate join

--- src/main/java/com/landvibe/landlog/service/MemberService.java
+++ src/main/java/com/landvibe/landlog/service/MemberService.java
@@ -7,9 +7,22 @@ import com.landvibe.landlog.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class MemberService {
+
+	private static final String EMAIL_REGEX =
+		"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+"
+			+ "(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\""
+			+ "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@"
+			+ "(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9]"
+			+ "(?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}"
+			+ "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:"
+			+ "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+	private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 	private final MemberRepository memberRepository;
 
 	public MemberService(MemberRepository memberRepository) {
@@ -17,9 +30,40 @@ public class MemberService {
 	}
 
 	public Long join(Member member) {
-		validateDuplicateMember(member); //중복 회원 검증
+		validateInvalidInput(member); //중복 회원 검증
 		memberRepository.save(member);
 		return member.getId();
+	}
+
+	private void validateInvalidInput(Member member) {
+		validateNoInput(member);
+		validateInvalidEmail(member.getEmail());
+		validateDuplicateMember(member);
+	}
+
+	private void validateNoInput(Member member) {
+		if (member.getName().equals("")) {
+			throw new IllegalArgumentException("이름을 입력해주세요.");
+		}
+		if (member.getPassword().equals("")) {
+			throw new IllegalArgumentException("비밀번호를 입력해주세요.");
+		}
+	}
+
+	private void validateInvalidEmail(String email) {
+		Optional.ofNullable(email)
+			.ifPresentOrElse(
+				e -> {
+					Matcher matcher = EMAIL_PATTERN.matcher(e);
+					if (!matcher.matches()) {
+						throw new IllegalArgumentException("이메일을 입력해주세요.");
+					}
+				},
+				() -> {
+					throw new IllegalArgumentException("이메일을 입력해주세요.");
+				}
+			);
+
 	}
 
 	private void validateDuplicateMember(Member member) {
@@ -28,7 +72,6 @@ public class MemberService {
 				throw new IllegalStateException("중복된 이메일입니다.");
 			});
 	}
-
 
 	public List<Member> findMembers() {
 		return memberRepository.findAll();
