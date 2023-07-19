f3970612d756eca9a716c312dbec0212e15db31e
leesc0839 <leesc0839@naver.com>
2023-07-19 22:58:47 +0900

    refactor(LoginService): LoginService 수정
    !member.getPassword().equals(loginForm.getPassword()) => 이 부분 메소드로 분리

--- src/main/java/com/landvibe/landlog/service/LoginService.java
+++ src/main/java/com/landvibe/landlog/service/LoginService.java
@@ -2,7 +2,6 @@ package com.landvibe.landlog.service;
 
 import com.landvibe.landlog.controller.LoginForm;
 import com.landvibe.landlog.domain.Member;
-import com.landvibe.landlog.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -10,22 +9,25 @@ import java.util.Optional;
 @Service
 public class LoginService {
 
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
 
-    public LoginService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    public LoginService(MemberService memberService) {
+        this.memberService = memberService;
     }
 
     public Long login(LoginForm loginForm) throws IllegalArgumentException {
-        Optional<Member> member = memberRepository.findByEmail(loginForm.getEmail());
+        Optional<Member> member = memberService.findOneByEmail(loginForm.getEmail());
         validateLogin(member, loginForm);
 
         return member.get().getId();
     }
 
-    private void validateLogin(Optional<Member> member, LoginForm loginForm) {
-        if (!member.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 이메일입니다."))
-                .getPassword().equals(loginForm.getPassword())) {
+    private void validateLogin(Optional<Member> member, LoginForm loginForm) throws IllegalArgumentException {
+        checkPassword(member.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 이메일입니다.")), loginForm);
+    }
+
+    private void checkPassword(Member member, LoginForm loginForm) {
+        if (!member.getPassword().equals(loginForm.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
     }
