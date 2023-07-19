27a379774a51c2f0e37f5c8b3d3cc8e543808db2
gimdongha <d11210920@gmail.com>
2023-07-19 12:58:54 +0900

    feat(Blog):create Blog form

---
+++ src/main/java/com/landvibe/landlog/domain/Blog.java
@@ -0,0 +1,50 @@
+package com.landvibe.landlog.domain;
+
+public class Blog {
+    private Long creatorId;
+    private Long id;
+    private String title;
+    private String contents;
+
+    public Blog() {
+    }
+
+    public Long getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(Long creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public Blog(Long creatorId, Long id, String title, String contents) {
+        this.creatorId = creatorId;
+        this.id = id;
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
