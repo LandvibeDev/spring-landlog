bb5432d35c1e6504bd16d3bbea8ed973f6d7db96
sheepysnoopy <qkffldlfdjsk12@naver.com>
2023-07-20 23:51:10 +0900

    .

--- src/main/java/com/landvibe/landlog/domain/Member.java
+++ src/main/java/com/landvibe/landlog/domain/Member.java
@@ -11,7 +11,6 @@ public class Member {
     }
 
     public Member(String name, String email, String password) {
-        this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
