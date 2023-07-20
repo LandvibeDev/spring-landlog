f2f14b6ec95c79fc8d8130cef3d1402cc823711b
kuk6933 <kuk6933@naver.com>
2023-07-21 00:35:38 +0900

    feat: add blogController blog CRUD

--- src/main/java/com/landvibe/landlog/controller/BlogController.java
+++ src/main/java/com/landvibe/landlog/controller/BlogController.java
@@ -1,28 +1,75 @@
 package com.landvibe.landlog.controller;
 
-import com.landvibe.landlog.domain.Member;
+import com.landvibe.landlog.domain.Blog;
+
+import com.landvibe.landlog.dto.BlogForm;
+import com.landvibe.landlog.dto.BlogUpdateForm;
+import com.landvibe.landlog.service.BlogService;
 import com.landvibe.landlog.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @RequestMapping("/blogs")
 @Controller
 public class BlogController {
 
     private final MemberService memberService;
+    private final BlogService blogService;
 
-    public BlogController(MemberService memberService) {
+    public BlogController(MemberService memberService, BlogService blogService) {
         this.memberService = memberService;
+        this.blogService = blogService;
     }
 
-    @GetMapping()
+    @GetMapping
     public String blog(@RequestParam Long creatorId, Model model) {
-        Member member = memberService.findById(creatorId);
-        model.addAttribute("name", member.getName());
+        List<Blog> blogs = blogService.findBlogsByCreatorId(creatorId);
+        model.addAttribute("creatorId", creatorId);
+        model.addAttribute("name", memberService.findById(creatorId).getName());
+        model.addAttribute("blogs", blogs);
         return "blog/blogList";
     }
 
+    @GetMapping("/new")
+    public String createBlogForm(@RequestParam Long creatorId, Model model) {
+        model.addAttribute("creatorId", creatorId);
+        model.addAttribute("name", memberService.findById(creatorId).getName());
+        return "blog/createBlogForm";
+    }
+
+    @PostMapping("/new")
+    public String createBlog(@RequestParam Long creatorId, BlogForm blogForm, RedirectAttributes redirectAttributes) {
+        Blog blog = new Blog(creatorId, blogForm.getTitle(), blogForm.getContent());
+        blogService.registerBlog(blog);
+        redirectAttributes.addAttribute("creatorId", creatorId);
+        return "redirect:/blogs";
+    }
+
+    @GetMapping("/update")
+    public String updateBlogForm(@RequestParam Long creatorId, @RequestParam Long blogId, Model model) {
+        Blog blog = blogService.findById(blogId);
+        model.addAttribute("creatorId", creatorId);
+        model.addAttribute("blog", blog);
+        model.addAttribute("name", memberService.findById(creatorId).getName());
+        return "blog/updateBlogForm";
+    }
+
+    @PostMapping("/update")
+    public String updateBlog(@RequestParam Long creatorId, BlogUpdateForm blogUpdateForm, RedirectAttributes redirectAttributes) {
+        blogService.updateBlog(blogUpdateForm);
+        redirectAttributes.addAttribute("creatorId", creatorId);
+        return "redirect:/blogs";
+    }
+
+    @PostMapping("/delete")
+    public String deleteBlog(@RequestParam Long blogId, @RequestParam Long creatorId, RedirectAttributes redirectAttributes) {
+        memberService.findById(creatorId); // creatorId 검증
+        blogService.deleteById(blogId);
+        redirectAttributes.addAttribute("creatorId", creatorId);
+        return "redirect:/blogs";
+    }
 }
