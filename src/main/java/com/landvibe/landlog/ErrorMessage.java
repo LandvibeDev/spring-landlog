26edd9072c0c781901516b7a17043a4d1f6f1f60
kuk6933 <kuk6933@naver.com>
2023-07-21 00:28:52 +0900

    feat: add error message

--- src/main/java/com/landvibe/landlog/ErrorMessage.java
+++ src/main/java/com/landvibe/landlog/ErrorMessage.java
@@ -3,6 +3,7 @@ package com.landvibe.landlog;
 public enum ErrorMessage {
     WRONG_PASSWORD("비밀번호가 틀렸습니다"),
     NO_USER("해당 정보와 일치하는 회원이 존재하지 않습니다"),
+    NO_BLOG("해당 정보와 일치하는 게시글이 존재하지 않습니다"),
     ALREADY_EXIST("이미 존재하는 회원입니다.");
 
     public String message;
