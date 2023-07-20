18f800037bd2e6022be5bb6767f74a4eb83dac7d
leesewon00 <dltpdnjs2000@gmail.com>
2023-07-20 13:40:59 +0900

    add blogService

--- src/main/java/com/landvibe/landlog/service/BlogService.java
+++ src/main/java/com/landvibe/landlog/service/BlogService.java
@@ -6,6 +6,8 @@ import com.landvibe.landlog.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 import static com.landvibe.landlog.ErrorMessage.*;
 
 @Service
@@ -23,48 +25,37 @@ public class BlogService {
             throw new IllegalArgumentException(EMPTY_CREATOR_ID.message);
         }
 
-        boolean exists = memberRepository.existsById(creatorId);
-        if(!exists){
-            throw new IllegalArgumentException(NO_MATCH_MEMBER_WITH_CREATOR_ID.message);
-        }
+        memberRepository.findById(creatorId).orElseThrow(() ->
+                new IllegalArgumentException(NO_MATCH_MEMBER_WITH_CREATOR_ID.message));
     }
 
     public void validateBlogId(Long blogId) {
         if (blogId == null || blogId == 0) {
             throw new IllegalArgumentException(EMPTY_BLOG_ID.message);
         }
-
-        boolean exists = blogRepository.existsById(blogId);
-        if(!exists){
-            throw new IllegalArgumentException(NO_MATCH_BLOG_WITH_BLOG_ID.message);
-        }
+        blogRepository.findById(blogId).orElseThrow(() ->
+                new IllegalArgumentException(NO_MATCH_BLOG_WITH_BLOG_ID.message));
     }
 
-    public Long write(Blog blog) throws IllegalArgumentException {
-        validateCreatorId(blog.getCreatorId());
+    public Long write(Blog blog) {
         blogRepository.save(blog);
         return blog.getId();
     }
 
-    public Long update(Blog blog) throws IllegalArgumentException {
-        validateCreatorId(blog.getCreatorId());
+    public Long update(Blog blog) {
         blogRepository.update(blog);
         return blog.getId();
     }
 
-    public List<Blog> findBlogsByCreatorId(Long creatorId) throws IllegalArgumentException {
-        validateCreatorId(creatorId);
+    public List<Blog> findBlogsByCreatorId(Long creatorId) {
         return blogRepository.findBlogsByCreatorId(creatorId);
     }
 
-    public Blog findById(Long blogId) {
-        Blog blog = blogRepository.findById(blogId)
-                .orElseThrow(() -> new IllegalArgumentException(NO_MATCH_BLOG_WITH_BLOG_ID.message));
-        return blog;
+    public Optional<Blog> findById(Long blogId) {
+        return blogRepository.findById(blogId);
     }
 
-    public Long deleteById(Long blogId) throws IllegalArgumentException {
-        validateCreatorId(findById(blogId).getCreatorId());
+    public Long deleteById(Long blogId) {
         Long id = blogRepository.deleteById(blogId);
         return id;
     }
