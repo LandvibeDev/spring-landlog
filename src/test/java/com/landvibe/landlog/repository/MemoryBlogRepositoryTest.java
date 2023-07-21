2f19d5530f5d0187b687f289d6bce9ab7a5f19c0
kuk6933 <kuk6933@naver.com>
2023-07-21 12:24:10 +0900

    refactor: delete useless code

--- src/test/java/com/landvibe/landlog/repository/MemoryBlogRepositoryTest.java
+++ src/test/java/com/landvibe/landlog/repository/MemoryBlogRepositoryTest.java
@@ -1,18 +1,14 @@
 package com.landvibe.landlog.repository;
 
 import com.landvibe.landlog.domain.Blog;
-import com.landvibe.landlog.domain.Member;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Optional;
 
-import static com.landvibe.landlog.ErrorMessage.NO_BLOG;
-import static com.landvibe.landlog.ErrorMessage.WRONG_PASSWORD;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class MemoryBlogRepositoryTest {
 
@@ -33,7 +29,7 @@ class MemoryBlogRepositoryTest {
         repository.save(blog);
 
         //then
-        Blog result = repository.findById(blog.getId()).get();
+        Blog result = repository.findByBlogId(blog.getId()).get();
         assertThat(result).isEqualTo(blog);
     }
 
@@ -44,21 +40,10 @@ class MemoryBlogRepositoryTest {
         repository.save(blog);
 
         //when
-        repository.deleteById(blog.getId());
+        repository.delete(blog.getId());
 
         //then
-        assertThat(repository.findById(blog.getId())).isEqualTo(Optional.empty());
-    }
-
-    @Test
-    void deleteById_예외() {
-        //given
-        Blog blog = new Blog(1L, "aa", "bb");
-        repository.save(blog);
-
-        Exception e = assertThrows(Exception.class,
-                () -> repository.deleteById(2L));
-        assertThat(e.getMessage()).isEqualTo(NO_BLOG);
+        assertThat(repository.findByBlogId(blog.getId())).isEqualTo(Optional.empty());
     }
 
     @Test
@@ -71,13 +56,10 @@ class MemoryBlogRepositoryTest {
         repository.save(blog2);
 
         //when
-        List<Blog> blogs = repository.findBlogByCreatorId(blog1.getCreatorId());
+        List<Blog> blogs = repository.findByCreatorId(blog1.getCreatorId());
 
         //then
         assertThat(blogs.get(0)).isEqualTo(blog1);
         assertThat(blogs.get(1)).isEqualTo(blog2);
     }
-
-
-
 }
