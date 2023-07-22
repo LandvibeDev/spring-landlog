786254b816f37be5f0030dee69f7a4cc59c088ac
gimdongha <d11210920@gmail.com>
2023-07-22 18:13:58 +0900

    refactor(ExceptionHandlingMethod):modify logic to throw exception when no such creatorId in member

--- src/main/java/com/landvibe/landlog/exceptions/ExceptionHandling.java
+++ src/main/java/com/landvibe/landlog/exceptions/ExceptionHandling.java
@@ -1,12 +1,17 @@
 package com.landvibe.landlog.exceptions;
 
+import com.landvibe.landlog.repository.MemberRepository;
+import com.landvibe.landlog.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 
 @Slf4j
 public class ExceptionHandling {
-    public static String exceptionHandlingMethod(Long creatorId) {
+    public static String exceptionHandlingMethod(Long creatorId, MemberService memberService) {
         try {
-            if(creatorId == null) throw new IllegalArgumentException("creatorId가 전달되지 않았습니다.");
+            if (memberService.findOne(creatorId).isEmpty()) {
+                throw new IllegalArgumentException("creatorId가 전달되지 않았습니다.");
+            }
         } catch (Exception e) {
             log.info("Exception 발생 : " + e.getClass().getName() + ", " + e.getMessage());
             return "home";
