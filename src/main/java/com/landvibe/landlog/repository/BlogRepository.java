26b3b84c33c0ce336f65d9c2d0ff5678dfd7f45b
genius00hwan <qnfn120@gmail.com>
2023-07-23 19:36:11 +0900

    fix (BlogRepository) : add functions

--- src/main/java/com/landvibe/landlog/repository/BlogRepository.java
+++ src/main/java/com/landvibe/landlog/repository/BlogRepository.java
@@ -1,4 +1,19 @@
 package com.landvibe.landlog.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.landvibe.landlog.domain.Blog;
+
 public interface BlogRepository {
+	void save(Blog blog);
+
+	void delete(Long blogId);
+
+	Blog update(Blog blog);
+
+	Optional<Blog> findByBlogId(Long blogId);
+
+	List<Blog> findByCreatorId(Long creatorId);
+
 }
