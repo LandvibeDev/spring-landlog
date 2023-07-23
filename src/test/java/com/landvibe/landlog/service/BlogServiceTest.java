60b42be43e9216374bd923497d357d0d3f3c8902
gimdongha <d11210920@gmail.com>
2023-07-23 12:00:27 +0900

    refactor(BlogServiceTest):modify all tests not to have dependencies to other classes

--- src/test/java/com/landvibe/landlog/service/BlogServiceTest.java
+++ src/test/java/com/landvibe/landlog/service/BlogServiceTest.java
@@ -10,49 +10,49 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class BlogServiceTest {
-
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
+    @InjectMocks
     BlogService blogService;
+    @Mock
     MemoryBlogRepository blogRepository;
+
+    Blog blog1 = new Blog(1L,1L, "title1", "content1");
+    Blog blog2 = new Blog(2L,1L, "title2", "content2");
+
+    List<Blog> blogs = Arrays.asList(blog1,blog2);
 
     @BeforeEach
     public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-        blogRepository = new MemoryBlogRepository();
         blogService = new BlogService(blogRepository);
     }
 
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-        blogRepository.clearStore();
-    }
 
     @Test
     @DisplayName("블로그 리스트 찾기 테스트")
     void findBlogs_whenGivenCreatorId_ReturnArrays() {
         //given
-        Member member = new Member(1L, "aa", "aa", "aa");
-        Blog blog1 = new Blog(1L, 1L, "title1", "contents1");
-        Blog blog2 = new Blog(1L, 2L, "title2", "contents2");
-        memberService.join(member);
+        Mockito.when(blogRepository.findAllByCreatorId(1L))
+                .thenReturn(blogs);
+
 
         //when
-        List<Blog> blogs = blogSetting(blog1, blog2);
 
         //then
-        assertThat(blogService.findBlogs(1L)).isEqualTo(blogs);
-        assertThat(blogService.findBlogs(2L)).isEmpty();
+        assertThat(blogs).isEqualTo(blogService.findBlogs(1L));
     }
 
 
@@ -60,73 +60,36 @@ class BlogServiceTest {
     @DisplayName("블로그 한개 찾기 테스트")
     void findOne_WhenGivenBLogIdAndCreatorId_ReturnBlog() {
         //given
-        Member member = new Member(1L, "aa", "aa", "aa");
-        Blog blog1 = new Blog(1L, 1L, "title1", "contents1");
-        Blog blog2 = new Blog(1L, 2L, "title2", "contents2");
-        memberService.join(member);
+        Mockito.when(blogRepository.findOneByBlogIdAndCreatorId(1L,1L))
+                .thenReturn(blog1);
 
         //when
-        List<Blog> blogs = blogSetting(blog1, blog2);
 
         //then
-        assertThat(blogService.findOne(1L, 1L)).isEqualTo(blog1);
-        assertThat(blogService.findOne(2L, 1L)).isEqualTo(blog2);
-
-        assertThat(blogService.findOne(1L, 1L)).isNotEqualTo(blog2);
+        assertThat(blog1).isEqualTo(blogService.findOne(1L,1L));
 
     }
 
+
+
     @Test
-    @DisplayName("블로그1을 블로그2와 같이 업데이트")
-    void updateBlog_WhenGivenBlogIdAndCreatorId() {
-        //given
-        Member member = new Member(1L, "aa", "aa", "aa");
-        Blog blog1 = new Blog(1L, 1L, "title1", "contents1");
-        Blog blog2 = new Blog(1L, 2L, "title2", "contents2");
-        memberService.join(member);
-        List<Blog> blogs = blogSetting(blog1, blog2);
+    @DisplayName("블로그 삭제 성공 테스트")
+    void delete_WhenGivenBlogIdAndCreatorID_ThenReturnTrue() {
+        Mockito.when(blogRepository.deleteBlog(1L,1L))
+                .thenReturn(true);
 
-        UpdateBlogForm updateBlogForm = new UpdateBlogForm(blog2.getTitle(), blog2.getContents());
 
-        //when
-        blogService.update(blog1.getId(), blog1.getCreatorId(), updateBlogForm);
-
-        //then
-        assertThat(blog1.getTitle()).isEqualTo(blog2.getTitle());
-        assertThat(blog1.getContents()).isEqualTo(blog2.getContents());
-
+        assertThat(blogService.delete(1L,1L)).isEqualTo(true);
     }
-
     @Test
-    @DisplayName("블로그 생성 테스트")
-    void create_WhenGivenBlog_ReturnTrue() {
-        //given
-        Member member = new Member(1L, "aa", "aa", "aa");
-        Blog blog1 = new Blog(1L, 1L, "title1", "contents1");
+    @DisplayName("블로그 삭제 실패 테스트")
+    void delete_WhenGivenBlogIdAndCreatorID_ThenThrowException() {
+        Mockito.when(blogRepository.deleteBlog(1L,2L))
+                .thenThrow(new IllegalArgumentException("해당 블로그가 존재하지 않습니다."));
 
-        //when
-        Long saveId = blogService.create(blog1);
 
-        //then
-        assertThat(blogRepository.findOneByBlogIdAndCreatorId(saveId, 1L)).isEqualTo(blog1);
-    }
-
-    @Test
-    void delete_WhenGivenBlogIdAndCreatorID_ThenReturnNull() {
-        //given
-        Member member = new Member(1L, "aa", "aa", "aa");
-        Blog blog1 = new Blog(1L, 1L, "title1", "contents1");
-        Blog blog2 = new Blog(1L, 2L, "title2", "contents2");
-        memberService.join(member);
-        List<Blog> blogs = blogSetting(blog1, blog2);
-
-        //when
-        blogService.delete(blog1.getId(), blog1.getCreatorId());
-
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
-                () -> blogRepository.findOneByBlogIdAndCreatorId(1L, 1L));//예외가 발생해야 한다.
-
-        //then
+        Exception e = assertThrows(Exception.class,
+                () -> blogRepository.deleteBlog(1L,2L));
         assertThat(e.getMessage()).isEqualTo("해당 블로그가 존재하지 않습니다.");
     }
 
