10235668a112afcc8303b14ef24ba36879d0c42f
leesewon00 <dltpdnjs2000@gmail.com>
2023-07-30 17:18:56 +0900

    1차 리팩토링

--- src/main/java/com/landvibe/landlog/controller/BlogApiController.java
+++ src/main/java/com/landvibe/landlog/controller/BlogApiController.java
@@ -3,6 +3,7 @@ package com.landvibe.landlog.controller;
 import com.landvibe.landlog.domain.Blog;
 import com.landvibe.landlog.service.BlogService;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -17,50 +18,30 @@ public class BlogApiController {
 
     @GetMapping()
     public List<Blog> list(@RequestParam long creatorId) {
-        try {
-            List<Blog> blogs = blogService.findBlogsByCreatorId(creatorId);
-            return blogs;
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
+        List<Blog> blogs = blogService.findBlogsByCreatorId(creatorId);
+        return blogs;
     }
 
     @PostMapping()
     public Blog create(@RequestParam long creatorId, @RequestBody Blog blog) {
-        try {
-            Blog write = blogService.write(blog);
-            return write;
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
+        Blog write = blogService.write(blog);
+        return write;
     }
 
     @GetMapping(value = "/{blogId}")
     public Blog update(@PathVariable long blogId, @RequestParam long creatorId) {
-        try {
-            Blog blog = blogService.findById(blogId);
-            return blog;
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
+        Blog blog = blogService.findById(blogId);
+        return blog;
     }
 
     @PutMapping(value = "/{blogId}")
     public Blog update(@PathVariable long blogId, @RequestParam long creatorId, @RequestBody Blog blog) {
-        try {
-            Blog update = blogService.update(blog);
-            return update;
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
+        Blog update = blogService.update(blog);
+        return update;
     }
 
     @DeleteMapping(value = "/{blogId}")
     public void delete(@PathVariable long blogId, @RequestParam long creatorId) {
-        try {
-            blogService.deleteById(blogId);
-        } catch (IllegalArgumentException e) {
-            return;
-        }
+        blogService.deleteById(blogId);
     }
 }
