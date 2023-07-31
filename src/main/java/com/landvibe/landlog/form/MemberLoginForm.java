cb1d1769cc4da97d51eccf06c02f33211d20280c
leesewon00 <dltpdnjs2000@gmail.com>
2023-07-31 11:56:58 +0900

    add @Builder @Setter @Getter

--- src/main/java/com/landvibe/landlog/form/MemberLoginForm.java
+++ src/main/java/com/landvibe/landlog/form/MemberLoginForm.java
@@ -1,22 +1,13 @@
 package com.landvibe.landlog.form;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class MemberLoginForm {
     private String email;
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
