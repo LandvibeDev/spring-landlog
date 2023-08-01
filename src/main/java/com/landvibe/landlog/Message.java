09940b94b1eeab045eb875f52701b30a6ce58165
임주민 <limj1213@gmail.com>
2023-08-01 13:07:05 +0900

    add(Message) : add constant message

--- src/main/java/com/landvibe/landlog/Message.java
+++ src/main/java/com/landvibe/landlog/Message.java
@@ -5,6 +5,7 @@ public enum Message {
     NO_INPUT_EMAIL("이메일을 입력해주세요"),
     NO_INPUT_PASSWORD("비밀번호를 입력해주세요"),
     NO_USER("존재하지 않는 회원입니다"),
+    NO_BLOG("존재하지 않는 블로그 게시물입니다"),
     DUPLICATE_EMAIL("이미 존재하는 이메일입니다."),
     DUPLICATE_NAME("이미 존재하는 이름입니다.");
 
