5e4d7d6290750d3f1ce1dae977bf32e242fdc7ca
임주민 <limj1213@gmail.com>
2023-08-01 12:53:49 +0900

    add(form) : add form to update blog

---
+++ src/main/java/com/landvibe/landlog/form/BlogUpdateForm.java
@@ -0,0 +1,31 @@
+package com.landvibe.landlog.form;
+
+public class BlogUpdateForm {
+    Long id;
+    String title;
+    String contents;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
