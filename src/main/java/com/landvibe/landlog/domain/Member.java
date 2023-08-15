0af57ca6216113f7f1605d0d4946a61d367f106c
leesc0839 <leesc0839@naver.com>
2023-08-15 18:05:03 +0900

    refactor(domain):
    @Builder, @Getter 사용. @Setter는 일부로 사용하지 않음

--- src/main/java/com/landvibe/landlog/domain/Member.java
+++ src/main/java/com/landvibe/landlog/domain/Member.java
@@ -1,54 +1,13 @@
 package com.landvibe.landlog.domain;
 
-public class Member {
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
+public class Member {
     private Long id;
     private String name;
     private String email;
     private String password;
-
-    protected Member() {
-    }
-
-    public static Member createMember(String name, String email, String password) {
-        Member member = new Member();
-        member.setName(name);
-        member.setEmail(email);
-        member.setPassword(password);
-
-        return member;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    private void setName(String name) {
-        this.name = name;
-    }
-
-    private void setEmail(String email) {
-        this.email = email;
-    }
-
-    private void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
 }
