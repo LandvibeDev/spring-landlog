8fb1f8be3f6ed5bf51b444a6ace0ff2ba9392a8a
leesewon00 <dltpdnjs2000@gmail.com>
2023-08-17 14:38:51 +0900

    add MemberApiController

--- src/main/java/com/landvibe/landlog/controller/MemberController.java
+++ src/main/java/com/landvibe/landlog/controller/MemberController.java
@@ -1,17 +1,8 @@
 package com.landvibe.landlog.controller;
 
-import com.landvibe.landlog.domain.Member;
-import com.landvibe.landlog.exception.LoginException;
-import com.landvibe.landlog.exception.MemberException;
-import com.landvibe.landlog.form.MemberJoinForm;
-import com.landvibe.landlog.form.MemberLoginForm;
 import com.landvibe.landlog.service.MemberService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/members")
@@ -27,34 +18,13 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
-    @PostMapping(value = "/new")
-    public String create(MemberJoinForm form) throws MemberException {
-        Member member = Member.builder()
-                .name(form.getName())
-                .email(form.getEmail())
-                .password(form.getPassword())
-                .build();
-
-        Long join = memberService.join(member);
-        return "redirect:/";
-    }
-
     @GetMapping()
-    public String list(Model model) {
-        List<Member> members = memberService.findMembers();
-        model.addAttribute("members", members);
+    public String list() {
         return "members/memberList";
     }
 
     @GetMapping(value = "/login")
     public String loginForm() {
         return "members/loginForm";
-    }
-
-    @PostMapping(value = "/login")
-    public String login(MemberLoginForm memberLoginForm, RedirectAttributes redirect) throws LoginException {
-        Long loginId = memberService.login(memberLoginForm.getEmail(), memberLoginForm.getPassword());
-        redirect.addAttribute("creatorId", loginId);
-        return "redirect:/blogs";
     }
 }
